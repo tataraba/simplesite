56ae43ce88129a29b2c6871fde256c3b47068bed
Mario Munoz <pythonbynight@gmail.com>
2023-02-26 23:14:37 -0800

    Add static dir and template files

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.config import Settings
 from app.routes import router
@@ -9,6 +10,8 @@ def get_app() -> FastAPI:
     """Create a FastAPI app with the specified settings."""
 
     app = FastAPI(**settings.fastapi_kwargs)
+
+    app.mount("/static", StaticFiles(directory=settings.STATIC_DIR), name="static")
 
     app.include_router(router)
 

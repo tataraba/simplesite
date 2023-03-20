02fce099bf068bcfc98fa2185e72f6f2c92c08eb
Mario Munoz <pythonbynight@gmail.com>
2023-03-19 20:20:39 -0700

    add taliwindcss command on startup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import logging
+import subprocess
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -8,19 +8,29 @@ from app.config import Settings
 from app.routes import router
 
 settings = Settings()
-logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    """Context manager for FastAPI app. It will run all code before `yield`
+    on app startup, and will run code after `yeld` on app shutdown.
+    """
 
-    print("Starting FastAPI...")
+    try:
+        subprocess.run([
+            "tailwindcss",
+            "-i",
+            str(settings.STATIC_DIR / "src" / "tw.css"),
+            "-o",
+            str(settings.STATIC_DIR / "css" / "main.css"),
+        ])
+    except Exception as e:
+        print(f"Error running tailwindcss: {e}")
+
     yield
-    print("closing")
+
 
 def get_app() -> FastAPI:
     """Create a FastAPI app with the specified settings."""
-
-
 
     app = FastAPI(lifespan=lifespan, **settings.fastapi_kwargs)
 

47cd8e3a6d3250d8e80f687687d6c6fde164a9ac
Mario Munoz <pythonbynight@gmail.com>
2023-03-24 18:55:43 -0700

    add tinydb wrapper

---
+++ app/crud.py
@@ -0,0 +1,58 @@
+import re
+
+from tinydb import Query, TinyDB
+
+from app.config import Settings
+
+settings = Settings()
+
+
+
+class CRUD:
+    def __init__(self, db: TinyDB | None = None, table: str | None = None):
+        self.db = db
+        self.query = Query()
+        self.table = table
+
+    def all_items(self):
+        if not self.db:
+            self.init_db
+        if self.table:
+            self.db = self.table
+        return self.db.all()
+
+    def find(self, key: str, value: str):
+        print(key, value)
+        q = getattr(self.query, key)
+        if not self.table:
+            return self.db.search(q == value)
+        return self.table.search(q == value)
+
+    def search(self, key: str, value: str):
+        q = getattr(self.query, key)
+        if not self.table:
+            return self.db.search(q.search(f"{value}+", flags=re.IGNORECASE))
+        return self.table.search(q.search(f"{value}+", flags=re.IGNORECASE))
+
+
+
+    @property
+    def init_db(self):
+        path = str(settings.DATA_DIR / "_data.json")
+        self.db = TinyDB(path, sort_keys=True, indent=4, separators=(",", ": "))
+        return self.db
+
+    @classmethod
+    def with_table(cls, table_name: str):
+        crud = cls()
+        _db = crud.init_db
+        _table = _db.table(table_name)
+        return cls(db=_db, table=_table)
+
+
+if __name__ == "__main__":
+    path = str(settings.DATA_DIR / "_data.json")
+
+    db = CRUD().with_table("artist_details")
+
+    print(db.search("name", "juliana"))
